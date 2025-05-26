a717913b42c16bc23f38f5ca2ea85aa50cdfb57b
Gasparsanchez334 <gasparsanchez334@gmail.com>
2025-05-26 19:36:52 -0300

    detalle Inmueble mejorado

--- src/components/detalleinmueble/DetalleDelInmueble.tsx
+++ src/components/detalleinmueble/DetalleDelInmueble.tsx
@@ -18,19 +18,22 @@ export default function DetalleDelInmueble({
     descrip, 
 }: Detalle){
     return <>
-            <label className="absolute top-30 px-8 text-4xl">{nom}</label>
-            <div className="p-1 mx-5 mt-15 bg-white rounded-xl shadow-md space-y-4">
-                <img src={imagen} className="h-150 object-cover rounded-lg"></img>
-                    <label className="px-6 text-2xl">Ciudad:{ciu}</label>
-                    <label className="px-6 text-2xl">Localidad:{loc}</label>
-                    <label className="px-6 text-2xl">Cantidad de huéspedes:{cantHuespedes}</label>
-                    <label className="px-6 grid">Descripcion:{descrip}</label>
-                <Link href= "/alquilar"> 
-                    <div className="">
-                      <button className=" bg-black hover:bg-gray-300 text-white w-200 rounded-xl w-1/6 h-13 mx-5">Alquilar</button>
-                    </div>
-                  </Link>
-            </div>
+        
             
+            <div className="p-1 mx-5 mt-15 bg-white rounded-xl shadow-md space-y-4">
+                <img src={imagen} className="h-150 object-cover rounded-lg "></img>    
+                <div className='px-9 gap-2 p-4'>
+                <label className="shadow-md bg-orange-100 px-4 py-2 rounded-md text-base ">Ciudad:{ciu}</label>
+                <label className="shadow-md bg-orange-100 px-4 py-2 rounded-md text-base">Localidad:{loc}</label>
+                <label className="shadow-md bg-orange-100 px-4 py-2 rounded-md text-base ">Cantidad de huéspedes:{cantHuespedes}</label>
+                <label className="absolute top-50 px-70 text-4xl">{nom}</label>
+                <label className="absolute  px-70 top-70 text-2xl">Descripcion:{descrip}</label>
+                </div>
+                <Link href= "/alquilar"> 
+                    <div className="absolute  px-210 top-120">
+                      <button className=" bg-black hover:bg-gray-300 text-white w-150 rounded-xl w-1/2 h-13 mx-5">Alquilar</button>
+                    </div>
+                </Link>
+            </div>
         </>
 }   
