c054322e165b0bd9ac8240be489b438bf1ad79e2
Gaspar Sanchez <78025076+Gasparsanchez334@users.noreply.github.com>
2025-06-12 08:07:38 -0300

    pago-tarjeta (#17)

--- src/pages/api/pagos.ts
+++ src/pages/api/pagos.ts
@@ -1,22 +1,37 @@
-import { getPago, updateStatePago } from '@/lib/db/pago'
-import { pagoExitoso } from '@/lib/pagos'
+import { updateStatePago, getPago, createPago } from '@/lib/db/pago'
+import { pagoExitoso } from '@/lib/pagos';
+import { estadoPago } from '@/types/estado-pago'
+import { pago } from '@/types/pago'
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { estadoPago } from '@/types/estado-pago' 
-
-
 export default async function handler(req:NextApiRequest,res:NextApiResponse){
-    const {method, query:{ id }, body: {estado}} = req
-    const pago = await getPago(id as string)
+    const method = req.method;
+const { id } = req.query;
+const { fullname, numerotarjeta, numeroseguridad, clienteid, solicitudid} = req.body;
+
+
     if (method == 'GET'){
-        
-       res.send(JSON.stringify(pago))
-    }
-  
-    if (method == 'POST'){
-        if (estado === estadoPago.exitoso){
-            await pagoExitoso(pago)
-        }
-        const resp = await updateStatePago(estado, id as string)
+        const resp = await getPago(id as string)
         res.send(JSON.stringify(resp))
+        return 
     }
-}
+    if (method == 'POST'){
+        console.log("Entro al POST");
+        console.log("Body recibido:", req.body);
+        const tarjetaValida = fullname === "Gaspar Sanchez" && numerotarjeta === "4447" && numeroseguridad === "677";
+        const fondosInsuficientes = fullname === "Gonzalo Bazan" && numerotarjeta === "4448" && numeroseguridad === "833";
+
+        if (tarjetaValida) {
+            const pago: pago = { id: null, numerotarjeta, numeroseguridad, fullname, clienteid, solicitudid, estado: estadoPago.exitoso};
+            await createPago(pago);
+            res.status(200).json({ message: 'El pago fue realizado con éxito' });
+  
+        } else if (fondosInsuficientes) {
+            res.status(400).json({ message: 'No se pudo realizar el pago por fondos insuficientes' });
+            }  
+            else {
+            res.status(400).json({ message: 'Tarjeta inválida' });
+        }
+        
+    }
+    }
+    
