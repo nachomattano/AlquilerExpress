b6084ed8da7ec8f355b0285e807f41024edb97f9
Juan Francisco Lorenzo <jfranlorenzo@gmail.com>
2025-06-17 21:00:11 -0300

    refactor: solicitudar reserva

--- src/components/DetalleInmueble/Reservar.tsx
+++ src/components/DetalleInmueble/Reservar.tsx
@@ -12,17 +12,7 @@ import toast from 'react-hot-toast';
 export default  function Reservar ({id}:{id:string}){
 
     const [rangoFechas, setRangoFechas] = useState<DateRange | undefined>();
-    const [isOp, setIsOp] = useState(false);
 
-    const [nombreAcom, setNombreAcom] = useState("");
-    const [dniAcom, setDniAcom] = useState("");
-    const [isSinCuenta, setIsSinCuenta] = useState(false);
-
-    const [fullName, setFullName] = useState("")
-    const [email, setEmail] = useState("")
-    const [emailAcom, setEmailAcom] = useState("")
-    const [dni, setDni] = useState("")
-    const [cantidadacompañantes, setCantidadAcompañantes] = useState("")
     const [disponibilidad, setDisponibilidad] = useState<DateRange[]>([]);
     const [acompanantes, setAcompanantes] = useState<string[]>([]);
     const [ inmueble, setInmueble ] = useState<inmueble|null|undefined>()
@@ -31,18 +21,21 @@ export default  function Reservar ({id}:{id:string}){
     const minFecha = addDays(hoy, 3)
     const [cantidadTotal, setCantidad] = useState<number> (1); 
     const [cantidadPermitida, setCantidadP] = useState<number> (1); 
+    const [ cantidadMenores, setCantidadMenores ] = useState<number>(0)
+    const [ cantidadMayores, setCantidadMayores ] = useState<number>(0)
 
 
     useEffect(() => {
         const usuarioActual = localStorage.getItem("user");
         if (usuarioActual) {
             setUsuario(JSON.parse(usuarioActual));
+        }else{
+            window.location.replace("/")
+            toast.error('No hay sesion iniciada actualmente')
         }
-        console.log('aquiii')
 
         const fetchClientes = async () => {
             try {
-                console.log("No asdasd")
                 const res = await fetch(`http://localhost:3000/api/inmueble/${id}/disponibilidad`, {
                     cache: "no-store",
                     headers: {
@@ -76,28 +69,12 @@ export default  function Reservar ({id}:{id:string}){
     }, []);
 
     const agregarAcompanante = async () => {
-        if (!emailAcom.trim()) return;
 
-        const acomp = await getUsuarioPorMail(emailAcom);
-
-        if (!acomp.user) {
-            toast.error("El email ingresado no está registrado en el sistema");
-            return;
-        }
-
-        setAcompanantes((prev) => [...prev, emailAcom]);
-        setCantidad(cantidadTotal +1)
-        setEmailAcom("");
-        setIsOp(false);
+        setCantidad(cantidadTotal + cantidadMayores)
     };
 
     const agregarAcompananteSinCuenta = () => {
-        if (!nombreAcom.trim()) return;
-
-        setAcompanantes((prev) => [...prev, nombreAcom]);
-        setCantidad(cantidadTotal +1)
-        setNombreAcom("");
-        setIsSinCuenta(false);
+        setCantidad(cantidadTotal + cantidadMenores)
     };
 
     const handleSubmit = async (e: React.FormEvent) => {
@@ -107,30 +84,24 @@ export default  function Reservar ({id}:{id:string}){
             toast.error("No se pudo identificar al usuario actual.");
             return;
         }
-        if(inmueble? inmueble.cantidadhuespedes : 1 <= parseInt(cantidadacompañantes || "0") + 1){
-           toast.error("")
+        if(cantidadPermitida < cantidadTotal+1){
+           toast.error("Maximo de huespedes superado")
            return; 
         }
 
-        const acompañantesId = await Promise.all(
-            acompanantes.map(async (acomp) => {
-                const acompañante = await getUsuarioPorMail(acomp);
-                return acompañante.user?.id ?? '1';
-            })
-        );
+        
 
         const res = await fetch('/api/solicitudes', {
             method: 'POST',
             body: JSON.stringify({
                 fechahasta: rangoFechas?.to,
                 fechadesde: rangoFechas?.from,
-                mail: email,
-                dni,
-                acompañantesid: acompañantesId,
-                nombre: fullName,
+                mail: usuario?.mail,
+                dni: usuario?.dni,
+                nombre: usuario?.nombre,
                 inmuebleid: id,
-                solicitante: usuario.id, 
-                cantidad: parseInt(cantidadacompañantes || "0") + 1,
+                solicitante: usuario?.id, 
+                cantidad: cantidadTotal+1,
                 monto: inmueble? inmueble.preciopordia : 3 * Math.floor(((rangoFechas && rangoFechas.to? rangoFechas.to.getTime(): 2) - (rangoFechas && rangoFechas?.from?  rangoFechas.from.getTime() : 1))/(1000 * 60 * 60 * 24))
             }),
             headers: { 'Content-Type': 'application/json' }
@@ -150,48 +121,7 @@ return <>
                                 <h2 className="text-center mt-4 text-2xl font-bold mb-4">Llenar formulario para solicitar reserva</h2>
                                 <div className="p-6 pt-0">
                                 <form onSubmit={handleSubmit} className="space-y-4">
-                                    <div className="space-y-2">
-                                    <label htmlFor="fullName" className="text-sm font-medium text-gray-700">
-                                    Nombre Completo
-                                    </label>
-                                    <input
-                                    id="fullName"
-                                    type="text"
-                                    placeholder="Juan Pérez"
-                                    required
-                                    value={fullName}
-                                    onChange={(e) => setFullName(e.target.value)}
-                                    className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"/>
-                                    </div>
-
-                                    <div className="space-y-2">
-                                    <label htmlFor="fullName" className="text-sm font-medium text-gray-700">
-                                    Email
-                                    </label>
-                                    <input
-                                    id="fullName"
-                                    type="text"
-                                    placeholder="...@gmail.com"
-                                    required
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                    className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"/>
-                                    </div>
-
-                                
-                                    <div className="space-y-2">
-                                    <label htmlFor="dni" className="text-sm font-medium text-gray-700">
-                                     DNI
-                                    </label>
-                                    <input
-                                        id="dni"
-                                        type="text"
-                                        placeholder="12345678"
-                                        required
-                                        value={dni}
-                                        onChange={(e) => setDni(e.target.value)}
-                                        className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"/>
-                                    </div>
+                                   
                                     
         
                                     
@@ -200,11 +130,14 @@ return <>
                                     <div className="mb-4">
                                         <h3 className="font-semibold text-lg text-green-700">Acompañantes agregados:</h3>
                                         <ul className="list-disc list-inside text-gray-800 mt-2">
-                                        {acompanantes.map((acomp, index) => (
-                                            <li key={index}>
-                                            {acomp.includes("@") ? `✔️ ${acomp} (con cuenta)` : `👤 ${acomp} (sin cuenta)`}
+                                      
+                                            <li key={1}>
+                                                Acompañantes mayores de edad {cantidadMayores}
                                             </li>
-                                        ))}
+
+                                            <li key={2}>
+                                                Acompañantes menores de edad {cantidadMenores}
+                                            </li>
                                         </ul>
                                     </div>
                                     )}
@@ -213,27 +146,31 @@ return <>
                                     
                                     
                                     
-                                    <div  className="space-y-4">
+                                    <div className="space-y-4">
+
+                    
                                             <div className="space-y-2">
-                                            <label htmlFor="fullName" className=" flex font-medium text-gray-700">
-                                            Email Acompañante
-                                            </label>
-                                            <input
-                                            id="fullName"
-                                            type="text"
-                                            placeholder="...@gmail.com"
-                                            value={emailAcom}
-                                            onChange={(e) => setEmailAcom(e.target.value)}
-                                            className="w-50 px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"/>
-                                            
-                                            <button
-                                            onClick={agregarAcompanante} // no necesita type="button" porque ya lo tiene implícito
-                                            type="button"
-                                            disabled= {cantidadPermitida < cantidadTotal+1}
-                                            className="ml-4 w-50 bg-black hover:bg-orange-500 text-white py-2 px-2 rounded-md"
-                                            >
-                                            Confirmar acompañante
-                                            </button>
+                                                <label htmlFor="cantidadMayores" className="flex font-medium text-gray-700">
+                                                    Cantidad De Acompañantes Mayores de Edad
+                                                </label>
+                                                <input
+                                                    id="cantidadMayores"
+                                                    type="number"
+                                                    min={0}
+                                                    max={cantidadTotal+1}
+                                                    placeholder="2"
+                                                    value={cantidadMenores}
+                                                    onChange={(e) => setCantidadMayores(parseInt(e.target.value))}
+                                                    className="w-50 px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                                                />
+                                                <button
+                                                    onClick={agregarAcompanante}
+                                                    type="button"
+                                                    disabled= {cantidadPermitida < cantidadTotal+1}
+                                                    className="ml-4 w-50 bg-black hover:bg-orange-500 text-white py-2 px-2 rounded-md"
+                                                >
+                                                    Confirmar acompañantes
+                                                </button>
                                             </div>
                                     </div>
 
@@ -241,36 +178,26 @@ return <>
 
                     
                                             <div className="space-y-2">
-                                                <label htmlFor="nombreAcom" className="flex font-medium text-gray-700">
-                                                    Nombre del Acompañante
+                                                <label htmlFor="cantidadMenores" className="flex font-medium text-gray-700">
+                                                    Cantidad De Acompañantes Menores de Edad
                                                 </label>
                                                 <input
-                                                    id="nombreAcom"
-                                                    type="text"
-                                                    placeholder="Ej: María Gómez"
-                                                    value={nombreAcom}
-                                                    onChange={(e) => setNombreAcom(e.target.value)}
-                                                    className="w-50 px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                                                />
-                                                <label htmlFor="dniAcom" className="flex font-medium text-gray-700">
-                                                    Dni del acompañante
-                                                </label>
-                                                <input
-                                                    id="dniAcom"
+                                                    id="cantidadMenores"
                                                     type="number"
-                                                    placeholder="Ej: 444444"
-                                                    value={dniAcom}
-                                                    onChange={(e) => setDniAcom(e.target.value)}
+                                                    min={0}
+                                                    max={cantidadTotal+1}
+                                                    placeholder="2"
+                                                    value={cantidadMenores}
+                                                    onChange={(e) => setCantidadMenores(parseInt(e.target.value))}
                                                     className="w-50 px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                                                 />
-
                                                 <button
                                                     onClick={agregarAcompananteSinCuenta}
                                                     type="button"
                                                     disabled= {cantidadPermitida < cantidadTotal+1}
                                                     className="ml-4 w-50 bg-black hover:bg-orange-500 text-white py-2 px-2 rounded-md"
                                                 >
-                                                    Confirmar acompañante
+                                                    Confirmar acompañantes
                                                 </button>
                                             </div>
                                     </div>
